66cf3df9b9adcac3f55cbfa4ebaa75cf975108d9
Benoit Viala <ben@viala.tech>
2023-12-04 02:12:38 +0100

    add GET results route to download data.csv file

--- app/main.py
+++ app/main.py
@@ -1,14 +1,16 @@
 from fastapi import FastAPI, status
+from fastapi.responses import FileResponse
 
 from app.models import database, Operation, Calc, CalcResult
 from app.calculator import compute
+import csv
+import datetime
 
 
 app = FastAPI(title="EasyRPN")
 
 @app.get("/", status_code=status.HTTP_200_OK)
 async def read_root():
-    #return await Operation.objects.all()
     return {"message": "Welcome to the EasyRPN API. Try route /docs for more infos"}
 
 @app.get("/api/healthchecker", status_code=status.HTTP_200_OK)
@@ -28,14 +30,25 @@ async def shutdown():
         await database.disconnect()
 
 
-#get : récupérer l'historique des opérations et résultats
-@app.get('/results', status_code=status.HTTP_200_OK)
-async def get_results():
-    return await Operation.objects.all()
+@app.get('/results', status_code=status.HTTP_200_OK, response_class=FileResponse)
+async def download_results_in_csv_file():
+    operations_list = await Operation.objects.all()
 
-#post : calculer resultats
+    date_time = datetime.datetime.now().strftime("%Y-%m-%d_%Hh%Mm%Ss")
+    file_name = f'{date_time}_results.csv'
+    file_path = f'/tmp/{file_name}'
+    with open(file_path, 'w', newline='') as csvfile:
+        csvwriter = csv.writer(csvfile, delimiter = ',')
+        csvwriter.writerow(["ID", "Operation", "Result", "Timestamp"])
+        for op in operations_list:
+            csvwriter.writerow([op.id, op.expression, op.result, op.timestamp])
+
+    response = FileResponse(file_path, media_type="text/csv")
+    response.headers["Content-Disposition"] = f"attachement; filename={file_name}"
+    return response
+
 @app.post("/calculate", status_code=status.HTTP_201_CREATED)
-async def calculate_result(operation: Calc) -> CalcResult:
+async def calculate_operation(operation: Calc) -> CalcResult:
     result: float
     expression: str
     #Resolve operation
